d2ca0821e5f6600f993b02fe080ab38b0a98d03a
1papaya <1love1papaya@gmail.com>
2021-12-01 00:37:24 -0500

    lower minzoom to 1

--- scripts/build-mbtiles.sh
+++ scripts/build-mbtiles.sh
@@ -1,6 +1,6 @@
 ./bin/tippecanoe --drop-densest-as-needed \
                  --force \
-                 --minimum-zoom=6 \
+                 --minimum-zoom=1 \
                  --maximum-zoom=11 \
                  --no-tile-compression \
                  --output-to-directory="./docs/" \
