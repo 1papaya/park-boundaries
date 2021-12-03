657071f5fbc1af6fa5f3acd2a40dcc80647ee316
1papaya <1love1papaya@gmail.com>
2021-12-02 22:30:20 -0500

    add --drop-rate=1 to mbtiles output

--- scripts/build-mbtiles.sh
+++ scripts/build-mbtiles.sh
@@ -1,5 +1,16 @@
+# ./bin/tippecanoe --drop-densest-as-needed \
+#                  --force \
+#                  --drop-rate=1 \
+#                  --minimum-zoom=1 \
+#                  --maximum-zoom=11 \
+#                  --no-tile-compression \
+#                  --output="./data/parks.mbtiles" \
+#                  ./data/boundaries.json \
+#                  ./data/points.json
+                 
 ./bin/tippecanoe --drop-densest-as-needed \
                  --force \
+                 --drop-rate=1 \
                  --minimum-zoom=1 \
                  --maximum-zoom=11 \
                  --no-tile-compression \
