ba9519f8031b5a845f73224c077fd8f390381dd7
1papaya <1love1papaya@gmail.com>
2022-01-06 12:08:25 -0700

    use wikidata english name as fallback if slugify fails on osm name (ie with cyrillic chars)

--- scripts/generate-index.ts
+++ scripts/generate-index.ts
@@ -7,9 +7,14 @@ import fs from "fs";
     (parksFeatureCollection) =>
       parksFeatureCollection.features.map((parkFeature) => ({
         name: parkFeature.properties.osm["name"],
-        slug: `${slugify(parkFeature.properties.wiki.country)}/${slugify(
-          parkFeature.properties.wiki.name
-        )}`,
+        slug: [
+          slugify(parkFeature.properties.wiki.country),
+          // if osm name is in other charset (ie cyrillic) use
+          // english name from wikidata
+          slugify(parkFeature.properties.osm["name"]) === ""
+            ? slugify(parkFeature.properties.wiki.name)
+            : slugify(parkFeature.properties.osm["name"]),
+        ].join("/"),
         wikidataId: parkFeature.properties.wiki.wikidataId,
         osmRelationId: parkFeature.id,
         bbox: turfBbox(parkFeature),
